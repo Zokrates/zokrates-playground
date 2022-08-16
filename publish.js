a98a964a66d8810955a208948b77f5309645bc8c
dark64 <darem966@gmail.com>
2022-08-16 14:05:47 +0200

    add cname file

--- publish.js
+++ publish.js
@@ -2,5 +2,6 @@ var ghpages = require("gh-pages");
 var fs = require("fs");
 
 fs.openSync("out/.nojekyll", "w");
+fs.copyFileSync("CNAME", "out/CNAME");
 
 ghpages.publish("out", { dotfiles: true });
